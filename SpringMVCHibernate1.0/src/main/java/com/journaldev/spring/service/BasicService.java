a0042c8e7aa190cd6daa909d95f76a65cd379f6d
pgy1 <1607974129@qq.com>
2016-04-09 23:50:48 +0800

    更新代码

---
+++ SpringMVCHibernate1.0/src/main/java/com/journaldev/spring/service/BasicService.java
@@ -0,0 +1,62 @@
+package com.journaldev.spring.service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by pengguangyu on 2016/1/21.
+ */
+
+public interface BasicService {
+
+//    public JdbcTemplate getJdbcTemplate();
+
+
+    /**
+     * 分页查询
+     */
+    public List pagedQuery(String sql, Object[] args, int[] argsType,
+                           int pageNum, int pageSize);
+
+    /**
+     * 支持大对象更新
+     */
+    public int update(String sql, Object[] args, int[] argsType);
+
+    /**
+     * 对于分页传入的是当前页的最开始数据量，以及结束数据量
+     *
+     * @param sql
+     * @param args
+     * @param argsType
+     * @param startIndex
+     * @param lastIndex
+     * @return
+     */
+    public List queryPage(String sql, Object[] args, int[] argsType,
+                          int startIndex, int lastIndex);
+
+    /**
+     * 用于返回所有数据量的操作
+     *
+     * @param sql
+     *            ：sql语句
+     * @param args
+     *            ：参数
+     * @param argsType
+     *            ：参数类型
+     * @return
+     */
+    public int getCntQuery(String sql, Object[] args, int[] argsType);
+
+    /**
+     * 找单个对象
+     */
+    public Map queryForSingle(String sql, Object[] args, int[] argsType);
+
+    /**
+     * 按列表返回查找结果
+     */
+    public List queryForList(String sql, Object[] args, int[] argsType);
+
+}
---
+++ SpringMVCHibernate1.1/target/SpringMVCHibernate/views/html.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2016/1/25 0025
+  Time: 下午 9:30
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+  ${html}
+</body>
+</html>
