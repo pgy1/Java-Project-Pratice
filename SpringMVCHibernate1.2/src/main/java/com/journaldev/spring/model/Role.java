4059f693bacaa05e7b632428c8cb321da3c3945f
pgy1 <1607974129@qq.com>
2016-04-11 23:45:11 +0800

    增加框架安全模块

---
+++ SpringMVCHibernate1.2/src/main/java/com/journaldev/spring/model/Role.java
@@ -0,0 +1,72 @@
+package com.journaldev.spring.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by jackson on 16/3/19.
+ */
+@Entity
+@Table(name="Roles")
+public class Role implements Serializable {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    int id;
+    String username;
+    String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role1 = (Role) o;
+
+        if (id != role1.id) return false;
+        if (!username.equals(role1.username)) return false;
+        return role.equals(role1.role);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + username.hashCode();
+        result = 31 * result + role.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
